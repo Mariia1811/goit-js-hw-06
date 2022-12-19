cf874447bb2ecfce160109dc50523d432ccc1c4b
Mariia1811 <115475304+Mariia1811@users.noreply.github.com>
2022-12-19 20:53:13 +0200

    add completed tasks

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const makeIngredientsList = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const ingredientsItemEl = document.createElement("li");
+    ingredientsItemEl.textContent = ingredient;
+    ingredientsItemEl.classList.add("item");
+    return ingredientsItemEl;
+  });
+};
+
+const liElements = makeIngredientsList(ingredients);
+
+document.querySelector("#ingredients").append(...liElements);
+
